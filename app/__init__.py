dc20ffdef3d52f3feedc208f18396c7fc06f7182
Mark Laskey <laskey.mark@hotmail.com>
2018-07-24 20:48:54 -0400

    added logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from config import Config
 
+import logging
+
 from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Logging
+handler = logging.FileHandler('/var/log/lexispringer_log/app.log')
+handler.setLevel(logging.ERROR)
+app.logger.addHandler(handler)
 
 # Flask Mail
 mail = Mail(app)
